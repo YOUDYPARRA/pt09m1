68b9ed3c9a54b3975258504eb39dcde717cf68de
youdyparra <jon.you@hotmail.com>
2022-08-20 14:54:19 -0500

    tarea cuatro

--- 04-EstructuraDeDatos-II/homework/homework.js
+++ 04-EstructuraDeDatos-II/homework/homework.js
@@ -3,17 +3,93 @@
 /*
 Implementar la clase LinkedList, definiendo los siguientes métodos:
   - add: agrega un nuevo nodo al final de la lista;
-  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
-  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+  - remove: elimina el último nodo de la lista y retorna su valor 
+  (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
+  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad:
+   el parámetro puede ser un valor o un callback.
+    En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; 
+    en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
   Ejemplo: 
   search(3) busca un nodo cuyo valor sea 3;
   search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
   En caso de que la búsqueda no arroje resultados, search debe retornar null.
 */
 
-function LinkedList() {}
+class LinkedList {
+  constructor(head){
+    if(head){
+      this.head=new Node(head);
+    }else{
+      this.head=null;
+    }
+    
+  }  
+}
+LinkedList.prototype.add=function(n){/*Agrega un nuevo nodo al final de la lista*/    
+  //recorrer el listado y colocar al ultimo*/
+  let nuevo=new Node(n);
+  console.log('INSTANCIA DE NODE: '+nuevo instanceof Node);
+  let tiene_algo=this.head
+  if(this.head){//si head tiene nodo recorrer nodos hasta encontrar un null y agregar
+      let actual=this.head;
+      while(actual.next){
+          actual=actual.next;
+      }
+      actual.next=nuevo;            
+  }else{//si head no tiene nodos, agregar nodo a head
+      this.head=new Node(nuevo);
+  }
+  };
+  LinkedList.prototype.remove=function(){/*ir al ultimo y antes de eliminar regresar su valor*/
+    //recorrer el listado 
+    //guardar el valor en variable temporal
+    //colocar en null el valor siguiente
+    //regresar el valor
+    if(!this.head){
+      return;
+    }else{
+      let actual=this.head;
+      apuntador=0;
+      while(actual.next){
+        apuntador++;
+          actual=next.siguiente;
+      }
+      actual=this.head;
+      contador=0;
+      let regresar;
+      while(actual.next){
+        contador++;
+        if(contador==apuntador){
+          regresar=actual.next;
+        }
+      }
+      actual.next=null;
+      return regresar;
+    }
 
-function Node(value) {}
+
+  };
+  LinkedList.prototype.search=function(v){//search(3) busca un nodo cuyo valor sea 3
+    //recorrer ccada elemento y verificar en cada dato si vale 3 en caso de ser numerico el valor
+
+  };
+  LinkedList.prototype.ultimo=function(){
+    let ultimo=head.siguiente;
+    while(ultimo){
+      ultimo=siguiente.siguiente
+      console.log(ultimo);
+    }
+    this.head
+  };
+
+class Node{
+  constructor(data){
+    return {
+      data:data,
+      next:null
+    };
+  }
+}
 
 /*
 Implementar la clase HashTable.
